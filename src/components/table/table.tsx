0554f8bd53bdef439ca8f03861ec60c90d6a8a3d
Akash <akashkarar26@gmail.com>
2023-05-08 17:43:39 +0530

    fix(edit-form): logic for updating row

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -54,9 +54,11 @@ const useTableData = () => {
       switch (action.type) {
         case "edit-row":
           // if (action.row) state.push(action.row);
-          return state.map((row) =>
+          const newState = state.map((row) =>
             row.id === action.row?.id ? action.row : row
           );
+          console.log(newState);
+          return newState;
         case "delete-row":
           return state.filter((row) => row.id != action.row?.id);
         default:
@@ -75,50 +77,106 @@ const useTableData = () => {
 };
 
 function EditForm(props: { data: Row; setRow: (arg0: Row) => void }) {
-  console.log({ EditFormData: props.data });
-  const dataKeys = useMemo(() => Object.keys(props.data), [props.data]);
+  const [form, setForm] = useReducer(
+    (current: Row, latest: Partial<Row>) => ({ ...current, ...latest }),
+    props.data
+  );
+
+  const dataKeys = useMemo(() => {
+    const keys = Object.keys(props.data);
+    keys.shift();
+    return keys;
+  }, [props.data]);
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        console.log(e.currentTarget.elements.namedItem("name"));
-        // setRow({});
-      }}
+    <div
+      className="border-b border-gray-900/10 pb-12 max-w-sm ml-1"
+      style={{ maxWidth: 440, marginLeft: "0.5rem" }}
     >
-      {dataKeys.map((key) => (
-        <label key={key} htmlFor={key}>
-          {key}:
-          <input
-            name={key}
-            id={key}
-            value={props.data[key as keyof Row]}
-            type="text"
-          />
-        </label>
-      ))}
-      <button type="submit">Update</button>
-    </form>
+      <h2 className="text-base font-semibold leading-7 text-gray-900">
+        Personal Information
+      </h2>
+      <p className="mt-1 text-sm leading-6 text-gray-600"></p>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          // console.log(e.currentTarget.id);
+          props.setRow(form);
+        }}
+        className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6"
+      >
+        {dataKeys.map((key, index) => (
+          <div key={index} className="sm:col-span-3">
+            <label
+              htmlFor={key}
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              {key}
+            </label>
+            <div className="mt-2">
+              {/* <div className='flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md'> */}
+              <input
+                name={key}
+                id={key}
+                value={form[key as keyof Row]}
+                onChange={(e) =>
+                  setForm({
+                    [key]: e.currentTarget.value,
+                  })
+                }
+                type="text"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              {/* </div> */}
+            </div>
+          </div>
+        ))}
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button
+            type="button"
+            className="text-sm font-semibold leading-6 text-gray-900"
+            style={{ margin: "0.5rem" }}
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            style={{
+              margin: "0.5rem",
+              paddingInline: "0.75rem",
+              paddingBlock: "0.5rem",
+              backgroundColor: "yellowgreen",
+            }}
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold  shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Update
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 function Table() {
   const tableData = useTableData();
-  console.log({ tableData });
+  // console.log({ tableData });
   const { rows } = tableData;
   const [editRow, setEditRow] = useState<Row>();
-  const [form, setForm] = useReducer(
-    (current: Row, latest: Row) => ({ ...current, ...latest }),
-    { email: "", id: -1, gender: "", name: "", phone: "" }
-  );
   return (
-    <div>
+    <div className="mx-auto px-6">
       {editRow && (
         <EditForm
           data={editRow}
-          setRow={(newRow) => tableData.updateRow(newRow)}
+          setRow={(newRow) => {
+            tableData.updateRow(newRow);
+            setEditRow(undefined);
+          }}
         />
       )}
+
+      {/* Table of user's details */}
+
       <table>
-        <caption>Add New Row</caption>
+        <caption className="font-medium">Add New Row</caption>
         <thead>
           <tr>
             <th>ID</th>
@@ -162,8 +220,6 @@ function Table() {
           ))}
         </tbody>
       </table>
-
-      {/* <button onClick={addRow}>Add Row</button> */}
     </div>
   );
 }
